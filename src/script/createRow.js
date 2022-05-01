faaf672e00024e4e6acbdfed44c6277173ead698
Oka R. Abdillah <hans.abdillh05@gmail.com>
2022-05-01 23:39:57 +0800

    update UI

--- src/script/createRow.js
+++ src/script/createRow.js
@@ -6,9 +6,9 @@ const createRow = (row, num, endpoint) => {
 	// Generate element
 	const element = `
         <tr class="${ num % 2 === 0 ? 'table-light' : '' }">
-			<td>${num}</td>
-			<td>${title}</td>
-            <td>${client}</td>
+            <td><small>${num}</small></td>
+            <td><small>${title}</small></td>
+            <td><small>${client}</small></td>
             <td>
 				<div class="d-flex gap-2">
 				 <img src="${endpoint}/public/${leaderPicture}" class="img-thumbnail rounded-circle border-none" width="50" alt="avatar" />
@@ -18,8 +18,8 @@ const createRow = (row, num, endpoint) => {
                   </span>
                 </div>
              </td>
-			 <td>${ new Date(startAt).toLocaleDateString() }</td>
-             <td>${ new Date(finishAt).toLocaleDateString() }</td>
+             <td><small>${ new Date(startAt).toLocaleDateString() }</small></td>
+             <td><small>${ new Date(finishAt).toLocaleDateString() }</small></td>
 			 <td>
                 <div class="progress">
                   <div class="progress-bar" role="progressbar" style="width: ${progress}%;" aria-valuenow="${progress}" aria-valuemin="0" aria-valuemax="100">${progress}%</div>
@@ -27,8 +27,8 @@ const createRow = (row, num, endpoint) => {
             </td>
             <td>
               <div class="d-flex gap-1">
-                <a class="btn btn-primary"><i class="fa fa-edit"></i></a>
-                <a class="btn btn-danger"><i class="fa fa-trash"></i></a>
+                <a class="btn btn-outline-primary"><i class="fa fa-edit text-sm"></i></a>
+                <a class="btn btn-outline-danger"><i class="fa fa-trash"></i></a>
               </div>
             </td>
            </tr> 
